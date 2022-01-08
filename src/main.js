dc26dc27ef39a8d57b8a297078860b98b3d42610
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-08 17:10:49 +0900

    register

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+import Koa from "koa";
 import Router from "koa-router";
 import api from "./api";
+import bodyParser from 'koa-bodyparser';
 
-const app = new Router();
+const app = new Koa();
+const router = new Router();
 
+router.use('/api', api.routes());
+router.get('/', ctx => {
+	ctx.body = "home";
+});
 
-app.use('/api', api.routes());
+app.use(bodyParser());
+
+app.use(router.routes).use(router.allowedMethods());
 app.listen(80, () => {
 	console.log("Listening to port 80")
 })
