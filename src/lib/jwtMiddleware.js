aff80c01a8ccd9c5a5966eb62c6c43a8854e2db2
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-08 20:14:54 +0900

    jwt token verify

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const jwtMiddleware = (ctx, next) => {
+	console.log('jwt!');
 	const token = ctx.cookies.get('access_tocken');
 	if(!token) return next(); //토큰 없음
 	try {
