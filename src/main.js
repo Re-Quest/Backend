c635e1846d8c3d6ee5bc4a2da581a172db8ee67c
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-08 19:01:48 +0900

    mongoose

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 import Koa from "koa";
 import Router from "koa-router";
 import api from "./api";
 import bodyParser from 'koa-bodyparser';
+const mongoose = require('mongoose')
 
 const app = new Koa();
 const router = new Router();
+
+const { PORT, MONGO_URI } = process.env;
+
+// mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI).then(() => {
+	console.log("Connected to MongoDB");
+}).catch(e => {
+	console.error(e);
+})
 
 router.use('/api', api.routes());
 router.get('/', ctx => {
@@ -13,7 +24,7 @@ router.get('/', ctx => {
 
 app.use(bodyParser());
 
-app.use(router.routes).use(router.allowedMethods());
-app.listen(80, () => {
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(PORT, () => {
 	console.log("Listening to port 80")
 })
